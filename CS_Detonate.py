26ee2c348b94ada1a39de3e60df2ee14f0481af9
Brian Uchino <buchino@frame10.com>
2026-03-04 23:03:34 +0000

    development

--- CS_Detonate.py
+++ CS_Detonate.py
@@ -12,9 +12,47 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'detonate_file_1' block
+    detonate_file_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def detonate_file_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("detonate_file_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.vaultId","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'detonate_file_1' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "limit": 50,
+                "is_confidential": True,
+                "vault_id": container_artifact_item[0],
+                "environment": "\"windows 10, 64-bit\"",
+                "comment": "Test file courtesy of Claude",
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("detonate file", parameters=parameters, name="detonate_file_1", assets=["cs"])
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
